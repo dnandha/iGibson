76b6ea07a6b734803cf4ae0645b0920548ad782d
dnandha <daljeet@gmx.net>
2021-07-01 16:09:50 +0200

    download assets even if asset folder already exists

--- gibson2/utils/assets_utils.py
+++ gibson2/utils/assets_utils.py
@@ -176,7 +176,8 @@ def download_assets():
     if not os.path.exists(os.path.dirname(gibson2.assets_path)):
         os.makedirs(os.path.dirname(gibson2.assets_path))
 
-    if not os.path.exists(gibson2.assets_path):
+    #if not os.path.exists(gibson2.assets_path):
+    if True:
         os.system(
             'wget -c --retry-connrefused --tries=5 --timeout=5 '
             'https://storage.googleapis.com/gibson_scenes/assets_igibson.tar.gz -O ~/tmp/assets_igibson.tar.gz')
